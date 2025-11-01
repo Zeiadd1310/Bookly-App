d2e0fd4087dd131d9b2aa85c3a65e6aa811ab51c
Zeiadd1310 <zeiadramadan6@gmail.com>
2025-11-01 13:46:00 +0200

    best seller item part 2

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -10,7 +10,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
           CustomAppBar(),
@@ -20,6 +20,7 @@ class HomeViewBody extends StatelessWidget {
             alignment: Alignment.centerLeft,
             child: Text('Best Seller', style: Styles.textStyle18),
           ),
+          const SizedBox(height: 20),
           BestSellerListViewItem(),
         ],
       ),
@@ -48,8 +49,18 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
           ),
-          Column(children: [
-               
+          const SizedBox(width: 30),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: Text(
+                  'Harry Potter and the Goblet of Fire',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20,
+                ),
+              ),
             ],
           ),
         ],
